22f00102b19ed61b898b28afebde91abfa869198
sondoshassan <sondos199628@gmail.com>
2020-06-29 21:16:42 +0300

    add github deploy

---
+++ starter-code/build/precache-manifest.3722ec55c4ff094990f24d383d6b2600.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9cb16ce2d3c2fa65c2131d9c0530d85",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "e8e5401f282d267a7881",
+    "url": "/resty/static/css/main.26024cff.chunk.css"
+  },
+  {
+    "revision": "0828aecb8d1951d6e3e4",
+    "url": "/resty/static/js/2.e37d5b6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.e37d5b6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8e5401f282d267a7881",
+    "url": "/resty/static/js/main.175b5f61.chunk.js"
+  },
+  {
+    "revision": "a53a0f6d519226fb9f59",
+    "url": "/resty/static/js/runtime-main.49bf7f19.js"
+  }
+]);
