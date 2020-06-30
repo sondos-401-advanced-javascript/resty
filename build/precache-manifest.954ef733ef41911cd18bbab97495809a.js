114bdfaf29ae7fc6ec983fbb58571169de791ba1
sondoshassan <sondos199628@gmail.com>
2020-06-30 23:18:43 +0300

    deploy

---
+++ build/precache-manifest.954ef733ef41911cd18bbab97495809a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f9591bde1e9a0f91831519c2f2aa32",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "e31f414b61af5902a800",
+    "url": "/resty/static/css/main.52f9d37c.chunk.css"
+  },
+  {
+    "revision": "82864f32bc6f2c19ae13",
+    "url": "/resty/static/js/2.7e60eb48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.7e60eb48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e31f414b61af5902a800",
+    "url": "/resty/static/js/main.c5b13cbe.chunk.js"
+  },
+  {
+    "revision": "a53a0f6d519226fb9f59",
+    "url": "/resty/static/js/runtime-main.49bf7f19.js"
+  }
+]);
